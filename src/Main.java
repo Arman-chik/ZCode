5b5ed34fb2149f3a7c94532f4067aa4bfd88a0db
Arman Oganesyan <oganesyanarman2019@gmail.com>
2026-05-20 08:45:51 +0300

    Финальный коммит. Добавление примеров с использованием всех новшеств в коде.

--- src/Main.java
+++ src/Main.java
@@ -13,18 +13,21 @@ import java.util.List;
 
 public class Main {
 public static void main(String[] args) throws IOException {
-        String input = new String(Files.readAllBytes(Paths.get("new_test_file.txt")), "UTF-8");
-        List<Token> tokens = new Lexer(input).tokenize();
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+    String input = new String(Files.readAllBytes(Paths.get("tests_files/PongGame.txt")), "UTF-8");
+    List<Token> tokens = new Lexer(input).tokenize();
+    for (Token token : tokens) {
+        System.out.println(token);
+    }
 
+    // tests_files/SnakeGame.txt
+    // tests_files/PainterApp.txt
+    // tests_files/PongGame.txt
 
-        Statement program = new Parser(tokens).parse();
-        System.out.println(program.toString());
-        program.accept(new FunctionsAdder());
-        // program.accept(new VariablePrinter()); // Вывод переменных задействованых в программе.
-        program.accept(new AssignValidator()); // Для запрета присваивания значений константам.
-        program.execute();
+    Statement program = new Parser(tokens).parse();
+    System.out.println(program.toString());
+    program.accept(new FunctionsAdder());
+    // program.accept(new VariablePrinter()); // Вывод переменных задействованых в программе.
+    program.accept(new AssignValidator()); // Для запрета присваивания значений константам.
+    program.execute();
     }
 }
